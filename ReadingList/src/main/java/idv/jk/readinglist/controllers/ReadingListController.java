98af196c428a56171eba1f6025cd519da2789281
javakidx <javakidx@gmail.com>
2015-10-29 01:42:20 +0800

    reader

---
+++ ReadingList/src/main/java/idv/jk/readinglist/controllers/ReadingListController.java
@@ -0,0 +1,48 @@
+package idv.jk.readinglist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import idv.jk.readinglist.entity.Book;
+import idv.jk.readinglist.repos.ReadingListRepository;
+
+@Controller
+@RequestMapping("/")
+public class ReadingListController
+{
+	private ReadingListRepository readingListRepository;
+	
+	@Autowired
+	public ReadingListController(ReadingListRepository readingListRepository)
+	{
+		this.readingListRepository = readingListRepository;
+	}
+	
+	@RequestMapping(value = "/{reader}", method = RequestMethod.GET)
+	public String readersBooks(@PathVariable("reader")String reader, Model model)
+	{
+		List<Book> readingList = readingListRepository.findByReader(reader);
+		
+		if (readingList != null)
+		{
+			model.addAttribute("books", readingList);
+		}
+		
+		return "readingList";
+	}
+	
+	@RequestMapping(value = "/{reader}", method = RequestMethod.POST)
+	public String addToReadingList(@PathVariable("reader")String reader, Book book)
+	{
+		book.setReader(reader);
+		readingListRepository.save(book);
+		
+		return "redirect:/{reader}";
+	}
+}
