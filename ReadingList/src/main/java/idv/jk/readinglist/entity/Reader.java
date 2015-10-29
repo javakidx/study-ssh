5dc52fcae74307c6497d3e527779bdff2d69b559
javakidx <javakidx@gmail.com>
2015-10-30 01:39:10 +0800

    security

---
+++ ReadingList/src/main/java/idv/jk/readinglist/entity/Reader.java
@@ -0,0 +1,93 @@
+package idv.jk.readinglist.entity;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+public class Reader implements UserDetails
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 911438894234554554L;
+
+	@Id
+	private String username;
+	private String fullname;
+	private String password;
+	
+	public String getFullname()
+	{
+		return fullname;
+	}
+
+	public void setFullname(String fullname)
+	{
+		this.fullname = fullname;
+	}
+
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
