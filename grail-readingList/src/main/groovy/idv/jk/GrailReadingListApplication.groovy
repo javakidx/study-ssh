26ccc1c4bf0faf6f28d3d4d9de36f9383f05daac
javakidx <javakidx@gmail.com>
2015-11-11 00:27:52 +0800

    modified

---
+++ grail-readingList/src/main/groovy/idv/jk/GrailReadingListApplication.groovy
@@ -0,0 +1,12 @@
+package idv.jk
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GrailReadingListApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run GrailReadingListApplication, args
+    }
+}
