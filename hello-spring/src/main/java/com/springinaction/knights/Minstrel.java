359d81bf3b5a9273c09a23fd591874d7720d3ec3
javakidx <javakidx@gmail.com>
2015-11-18 00:56:04 +0800

    hello aop

---
+++ hello-spring/src/main/java/com/springinaction/knights/Minstrel.java
@@ -0,0 +1,17 @@
+package com.springinaction.knights;
+
+/**
+ * Created by bioyang on 2015/11/18.
+ */
+public class Minstrel
+{
+    public void singBeforeQuest()
+    {
+        System.out.println("Fa la la; The knight is so brave!");
+    }
+
+    public void singAfterQuest()
+    {
+        System.out.println("Tee hee he; The brave knigt did embark on a quest!");
+    }
+}
