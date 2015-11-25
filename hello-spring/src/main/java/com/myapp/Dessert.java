3de6851801b4a3c6c92b4d9a0b215fd6527423a4
javakidx <javakidx@gmail.com>
2015-11-26 01:18:18 +0800

    qualifier

---
+++ hello-spring/src/main/java/com/myapp/Dessert.java
@@ -0,0 +1,8 @@
+package com.myapp;
+
+/**
+ * Created by bioyang on 2015/11/25.
+ */
+public interface Dessert
+{
+}
---
+++ hello-spring/src/main/java/com/myapp/IceCream.java
@@ -0,0 +1,12 @@
+package com.myapp;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by bioyang on 2015/11/25.
+ */
+@Component
+@Cold
+public class IceCream implements Dessert
+{
+}
